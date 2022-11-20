d3318bc21104c5ad61e1b9d682b738308eb829aa
Trevor-Muthoka <trevor2001.tm@gmail.com>
2022-11-21 02:09:41 +0300

    Trial stuff don't merge yet

--- app/src/main/java/com/example/mycounsellor/GetStarted.kt
+++ app/src/main/java/com/example/mycounsellor/GetStarted.kt
@@ -1,28 +1,20 @@
 package com.example.mycounsellor
 
 import android.content.Intent
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import android.os.Bundle
 import android.widget.Button
-import com.example.mycounsellor.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
+class GetStarted : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_started)
+        title = "GetStarted"
         val button = findViewById<Button>(R.id.getstarted)
         button.setOnClickListener {
             val intent = Intent(this, Patient_homepage::class.java)
             startActivity(intent)
         }
-    }
 
+    }
 }
--- app/src/main/java/com/example/mycounsellor/Patient_homepage.kt
+++ app/src/main/java/com/example/mycounsellor/Patient_homepage.kt
@@ -1,11 +1,18 @@
 package com.example.mycounsellor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_patient_homepage.*
 
 class Patient_homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_homepage)
+        viewAppointment.isClickable = true
+        viewAppointment.setOnClickListener {
+            val intent = Intent(this, MyAppointment::class.java)
+            startActivity(intent)
+        }
     }
 }
