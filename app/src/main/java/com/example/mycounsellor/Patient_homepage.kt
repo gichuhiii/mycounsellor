7cad42f16b06026144b8cd11c256ec62ff826298
Trevor-Muthoka <84325467+Trevor-Muthoka@users.noreply.github.com>
2022-11-28 10:37:48 +0300

    Update Patient_homepage.kt

--- app/src/main/java/com/example/mycounsellor/Patient_homepage.kt
+++ app/src/main/java/com/example/mycounsellor/Patient_homepage.kt
@@ -6,13 +6,9 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_patient_homepage.*
 
 class Patient_homepage : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_homepage)
-        viewAppointment.isClickable = true
-        viewAppointment.setOnClickListener {
-            val intent = Intent(this, MyAppointment::class.java)
-            startActivity(intent)
-        }
+
     }
 }
